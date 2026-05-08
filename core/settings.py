4b0e562caa20818b02ad0ab80fd51a5b69ef8534
edcleyssonalves <edcleyssonalves@gmail.com>
2026-05-08 20:21:36 -0300

    adiciona filtros do django-rql a customers

--- core/settings.py
+++ core/settings.py
@@ -185,7 +185,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
-    # 'DEFAULT_PERMISSION_CLASSES': [
-    #     'rest_framework.permissions.IsAuthenticated',
-    # ]
+    'DEFAULT_FILTER_BACKENDS': [
+        'dj_rql.drf.RQLFilterBackend'
+    ],
 }
