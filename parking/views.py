c35714d75c6a2342410e630644e34ab83c39eb8a
edcleyssonalves <edcleyssonalves@gmail.com>
2026-05-08 20:48:01 -0300

    adiciona filtros do django-rql a app parking

--- parking/views.py
+++ parking/views.py
@@ -2,6 +2,7 @@ from rest_framework.permissions import DjangoModelPermissions
 from rest_framework import viewsets
 
 from core.permissions import IsOwnerOfVehicleOrRecord
+from parking.filters import ParkingSpotFilterClass, ParkingRecordFilterClass
 from parking.models import ParkingRecord, ParkingSpot
 from parking.serializers import ParkingRecordSerializer, ParkingSpotSerializer
 
@@ -9,12 +10,14 @@ from parking.serializers import ParkingRecordSerializer, ParkingSpotSerializer
 class ParkingSpotViewSet(viewsets.ModelViewSet):
     queryset = ParkingSpot.objects.all()
     serializer_class = ParkingSpotSerializer
+    rql_filter_class = ParkingSpotFilterClass
     permission_classes = [DjangoModelPermissions]
 
 
 class ParkingRecordViewSet(viewsets.ModelViewSet):
     queryset = ParkingRecord.objects.all()
     serializer_class = ParkingRecordSerializer
+    rql_filter_class = ParkingRecordFilterClass
     permission_classes = [DjangoModelPermissions, IsOwnerOfVehicleOrRecord]
 
     def get_queryset(self):
