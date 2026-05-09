977687bd426f2073e60bb221a84785600de908b3
edcleyssonalves <edcleyssonalves@gmail.com>
2026-05-09 03:14:51 -0300

    adiciona drf-spectacular ao projeto, implementa rotas de documentação do projeto

--- core/settings.py
+++ core/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_simplejwt',
+    'drf_spectacular',
     'authentication',
     'customers',
     'vehicles',
@@ -188,4 +189,11 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': [
         'dj_rql.drf.RQLFilterBackend'
     ],
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Service Parking API',
+    'DESCRIPTION': 'API Project for Parking Services',
+    'VERSION': '1.0.0',
 }
